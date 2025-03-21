0c2e17d0fffaae4b0b72f941e0914c5112d8480b
Rahma Allam <157872715+Reem19-15@users.noreply.github.com>
2025-03-21 21:41:44 +0200

    Update Home copy.jsx

--- src/pages/Home copy.jsx
+++ src/pages/Home copy.jsx
@@ -1,12 +1,23 @@
 import React, { useState } from "react";
-import movie1 from "../assets/movie1.jpg";
-import movie2 from "../assets/movie2.jpg";
-import movie3 from "../assets/movie3.jpg";
+import SideBar from "../components/sidebar";
+import SearchBar from "../components/searchNav";
 
 const movies = [
-  { image: movie1, title: "Venom 1", description: "Short movie description here..." },
-  { image: movie2, title: "MOVIE 2", description: "Short movie description here..." },
-  { image: movie3, title: "MOVIE 3", description: "Short movie description here..." }
+  {
+    image: require("../assets/movie1.jpg"),
+    title: "Venom 1",
+    description: "Short movie description here...",
+  },
+  {
+    image: require("../assets/movie2.jpg"),
+    title: "MOVIE 2",
+    description: "Short movie description here...",
+  },
+  {
+    image: require("../assets/movie3.jpg"),
+    title: "MOVIE 3",
+    description: "Short movie description here...",
+  },
 ];
 
 const Home = () => {
@@ -17,13 +28,19 @@ const Home = () => {
   };
 
   const prevMovie = () => {
-    setIndex((prevIndex) => (prevIndex - 1 + movies.length) % movies.length);
+    setIndex((prevIndex) => (prevIndex - 1) % movies.length);
   };
 
   return (
     <div className="home-container">
-      <div className="movie-card  w-[90%] h-auto md:h-[500px] object-cover">
-        <img src={movies[index].image} alt={movies[index].title} className="movie-image" />
+      <SideBar />
+      <SearchBar />
+      <div className="movie-card w-[90%] h-auto md:h-[500px] object-cover">
+        <img
+          src={movies[index].image}
+          alt={movies[index].title}
+          className="movie-image"
+        />
         <div className="movie-content">
           <h1>{movies[index].title}</h1>
           <p>{movies[index].description}</p>
@@ -32,16 +49,23 @@ const Home = () => {
             <button className="favorite">More info</button>
           </div>
         </div>
-        <button className="prev-btn" onClick={prevMovie}>❮</button>
-        <button className="next-btn" onClick={nextMovie}>❯</button>
+        <button className="prev-btn" onClick={prevMovie}>
+          ❮
+        </button>
+        <button className="next-btn" onClick={nextMovie}>
+          ❯
+        </button>
       </div>
       <div className="relative w-full">
-          <div className="absolute mt-5 top-1/2 w-full border-t border-gray-600 "></div>
-          <div className="relative inline-block bg-purple-600 text-white ml-20 px-6 py-2 text-lg  rounded-tl-lg rounded-br-lg mt-10 ml-4">Movie</div> 
+        <div className="absolute mt-5 top-1/2 w-full border-t border-gray-600 "></div>
+        <div className="relative inline-block bg-purple-600 text-white ml-20 px-6 py-2 text-lg  rounded-tl-lg rounded-br-lg mt-10 ml-4">
+          Movie
+        </div>
       </div>
-      <div className="mt-5 white"><h1>New Release </h1></div>
+      <div className="mt-5 white">
+        <h1>New Release </h1>
+      </div>
     </div>
-    
   );
 };
 
